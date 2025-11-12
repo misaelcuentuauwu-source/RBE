e51b9fb930688722714e22848971b60f6887406d
Rodavlas7 <shavitagarciab@gmail.com>
2025-11-12 08:00:53 -0800

    a

--- bd.sql
+++ bd.sql
@@ -1,9 +1,21 @@
 CREATE DATABASE RBE;
 use RBE;
 
-CREATE Table modelo(
+CREATE TaBLE modelo(
     numero INT PRIMARY KEY,
     nombre VARCHAR(30) NOT NULL,
     numAsientos INT NOT NULL,
-    anio 
+    anio INT NOT NULL,
+    capacidad INT NOT NULL,
+    marca VARCHAR(20) NOT NULL
 );
+
+CREATE TABLE conductor(
+    registro INT PRIMARY KEY,
+    conNombre VARCHAR(30) NOT NULL,
+    conPrimerApell VARCHAR(30) NOT NULL,
+    conSegApell VARCHAR(30) NOT NULL,
+    licNumero VARCHAR(14) NOT NULL,
+    licVencimiento DATE NOT NULL
+);
+
